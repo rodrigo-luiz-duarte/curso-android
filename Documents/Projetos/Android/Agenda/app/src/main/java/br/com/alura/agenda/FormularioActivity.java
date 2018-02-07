f34fe56a1611f5a56d4a4fc335073cac4d6af528
rodrigo <rodrigo.luizduarte@gmail.com>
2018-02-06 23:27:49 -0200

    Terceiro exercício.

--- Documents/Projetos/Android/Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ Documents/Projetos/Android/Agenda/app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -2,6 +2,8 @@ package br.com.alura.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,17 +15,25 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        this.registreListenerBtnSalvar();
     }
 
-    private void registreListenerBtnSalvar() {
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_btn_salvar);
 
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
                 Toast.makeText(FormularioActivity.this, "Aluno salvo com sucesso!", Toast.LENGTH_SHORT).show();
-            }
-        });
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
